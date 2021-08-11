aeea8cf3543d7d8d3092d6612ecee9fbcbada704
Jerry Leong <80385434+jerryleongjunfai@users.noreply.github.com>
2021-08-11 13:23:59 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import PIL
+from PIL import Image
+from tkinter.filedialog import *
+
+file_path = askopenfilename()
+img = PIL.Image.open(file_path)
+myheight, mywidth = img.size
+
+img = img.resize((myheight, mywidth),PIL.Image.ANTIALIAS)
+save_path = asksaveasfilename()
+
+img.save(save_path+"_compressed.jpg")
+
